c84c7d250e683d323bd0574c0a6eda3bf0888948
diwata2021 <diwata2021@github.com>
2024-07-12 18:23:25 +0800

    add contract support loss value

--- contracts/AspectMap.sol
+++ contracts/AspectMap.sol
@@ -8,6 +8,8 @@ import "../node_modules/@openzeppelin/contracts/access/Ownable.sol";
 
 contract AspectMap is ERC721, ERC721Burnable, Ownable {
     uint256 private _nextTokenId;
+    string constant public CurrentLossPre = "#currentLoss";
+    string constant public TransferTimesPre = "#transferTimes";
 
     constructor()
     ERC721("AspectMap", "AM")
@@ -17,5 +19,22 @@ contract AspectMap is ERC721, ERC721Burnable, Ownable {
     function safeMint(address to) public onlyOwner {
         uint256 tokenId = _nextTokenId++;
         _safeMint(to, tokenId);
+    }
+
+    function getLossValue(address aspectId, uint256 index) public returns (uint256 lossValue) {
+        bytes memory contextKey = abi.encodePacked(aspectId, address(this), index, CurrentLossPre);
+        (bool success, bytes memory returnData) = address(0x64).call(contextKey);
+        // 如果不存在key，是新的
+        if (!success) {
+            return 100;
+        }
+        return uint256(returnData);
+    }
+
+    function updateLossValue(address aspectId, uint256 index, uint256 newLossValue) public onlyOwner returns (string memory validationData) {
+        bytes memory contextKey = abi.encodePacked(aspectId, address(this), index, CurrentLossPre);
+        bytes memory contextValue = abi.encode(contextKey, newLossValue);
+        (bool success,) = address(0x66).call(contextValue);
+        return success;
     }
 }
